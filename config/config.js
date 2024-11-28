13dd57f423933c84e048c87246b72d66931e4177
amjadkhanniazi <amjadkhanniazi010@gmail.com>
2024-11-28 15:27:09 +0500

    forth commit

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 export default {
-    MongoURL: 'mongodb+srv://amjad:myacount13@mongotest.rqx1a.mongodb.net/TaskManage'
+    MongoURL: 'mongodb+srv://amjad:<Password>@mongotest.rqx1a.mongodb.net/TaskManage'
 }
